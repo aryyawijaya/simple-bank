f5585e67607c809466db646c151be16a595c1122
aryya <aryyawijaya@gmail.com>
2023-12-03 16:40:59 +0700

    feat: load config with viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,21 +6,27 @@ import (
 	"os"
 	"testing"
 
+	"github.com/aryyawijaya/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	driverName     = "postgres"
-	dataSourceName = "postgresql://root:secretpassword@localhost:5433/simple_bank?sslmode=disable"
-)
+// const (
+// 	driverName     = "postgres"
+// 	dataSourceName = "postgresql://root:secretpassword@localhost:5433/simple_bank?sslmode=disable"
+// )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	var err error
+	// var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatalf("Cannot load config: %v\n", err)
+	}
 
-	testDB, err = sql.Open(driverName, dataSourceName)
+	// testDB, err = sql.Open(driverName, dataSourceName)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalf("Cannot connect to database: %v", err)
 	}
--- main.go
+++ main.go
@@ -6,17 +6,23 @@ import (
 
 	"github.com/aryyawijaya/simple-bank/api"
 	mydb "github.com/aryyawijaya/simple-bank/db/sqlc"
+	"github.com/aryyawijaya/simple-bank/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	driverName     = "postgres"
-	dataSourceName = "postgresql://root:secretpassword@localhost:5433/simple_bank?sslmode=disable"
-	serverAddress  = "localhost:8080"
-)
+// const (
+// 	driverName     = "postgres"
+// 	dataSourceName = "postgresql://root:secretpassword@localhost:5433/simple_bank?sslmode=disable"
+// 	serverAddress  = "localhost:8080"
+// )
 
 func main() {
-	conn, err := sql.Open(driverName, dataSourceName)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatalf("Cannot load config: %v\n", err)
+	}
+	// conn, err := sql.Open(driverName, dataSourceName)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatalf("Cannot connect to database: %v\n", err)
 	}
@@ -24,7 +30,8 @@ func main() {
 	store := mydb.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	// err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatalf("Cannot start the server: %v\n", err)
 	}
